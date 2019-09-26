ebdd90a5fbe1c30df68152545de97f0862be16f0
Jason Meeks <jmeeks@clearwaveinc.com>
2019-09-25 22:44:25 -0400

    Initial commit

---
+++ Pulse8TestApp/MemberCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulse8TestApp
+{
+    public class MemberCategory
+    {
+        public int MemberID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? MostSevereDiagnosisID { get; set; }
+        public string MostSevereDiagnosisDescription { get; set; }
+        public int? DiagnosisCategoryID { get; set; }
+        public string CategoryDescription { get; set; }
+        public int? CategoryScore { get; set; }
+        public int IsMostSevereCategory { get; set; }
+    }
+}
---
+++ Pulse8TestApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulse8TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string testString;
+                Console.Write("\nEnter Member ID:");
+                testString = Console.ReadLine();
+
+                using (var context = new Pulse8TestDBEntities())
+                {
+                    var memberCategories = context.Database.SqlQuery<MemberCategory>(
+                        @"SELECT * FROM (
+                            SELECT 
+	                            m.MemberID
+	                            ,m.FirstName
+	                            ,m.LastName
+	                            ,d.DiagnosisID as MostSevereDiagnosisID
+	                            ,d.DiagnosisDescription as MostSevereDiagnosisDescription
+	                            ,dc.DiagnosisCategoryID
+	                            ,dc.CategoryDescription
+	                            ,dc.CategoryScore
+	                            ,CASE WHEN dc.DiagnosisCategoryID IS NULL THEN 1 ELSE 
+		                            CASE WHEN ROW_NUMBER() OVER (PARTITION BY m.MemberId ORDER BY dc.DiagnosisCategoryID ASC) = 1 THEN 1 ELSE 0 END
+	                            END AS IsMostSevereCategory
+	                            ,ROW_NUMBER() OVER (PARTITION BY m.MemberId, dc.DiagnosisCategoryID ORDER BY d.DiagnosisID ASC) as rownum
+                            FROM Member m
+	                            LEFT JOIN MemberDiagnosis md ON m.MemberID = md.MemberID
+	                            LEFT JOIN DiagnosisCategoryMap dcm ON dcm.DiagnosisID = md.DiagnosisID
+	                            LEFT JOIN DiagnosisCategory dc ON dc.DiagnosisCategoryID = dcm.DiagnosisCategoryID
+	                            LEFT JOIN Diagnosis d ON d.DiagnosisID = md.DiagnosisID
+                            GROUP  BY m.MemberID, 
+	                            m.FirstName, 
+	                            m.LastName, 
+	                            dc.DiagnosisCategoryID, 
+	                            dc.CategoryDescription, 
+	                            dc.CategoryScore, 
+	                            d.DiagnosisID, 
+	                            d.DiagnosisDescription) t 
+                            WHERE  t.rownum = 1
+                            AND t.MemberID = {0}", testString
+                            ).ToList();
+
+                    Console.WriteLine($"\nMemberID | " +
+                                      $"FirstName | " +
+                                      $"LastName | " +
+                                      $"MostSevere | " +
+                                      $"MostSevere       | " +
+                                      $"Diagnosis  | " +
+                                      $"Category    | " +
+                                      $"Category | " +
+                                      $"IsMostSevere");
+
+                    Console.WriteLine($"         | " +
+                                      $"          | " +
+                                      $"         | " +
+                                      $"DiagID     | " +
+                                      $"DiagDesc         | " +
+                                      $"CategoryID | " +
+                                      $"Description | " +
+                                      $"Score    | " +
+                                      $"Category");
+
+                    Console.WriteLine($"---------+-----------+----------+------------+------------------+----------------+---------+----------+-------------");
+
+                    foreach (var member in memberCategories)
+                    {
+                        Console.WriteLine($"{member.MemberID,8} | " +
+                                          $"{member.FirstName,9} | " +
+                                          $"{member.LastName,8} | " +
+                                          $"{member.MostSevereDiagnosisID,10} | " +
+                                          $"{member.MostSevereDiagnosisDescription,16} | " +
+                                          $"{member.DiagnosisCategoryID,10} | " +
+                                          $"{member.CategoryDescription,11} | " +
+                                          $"{member.CategoryScore,8} | " +
+                                          $"{member.IsMostSevereCategory,12}");
+                    }
+                }
+            }
+        }
+    }
+}
